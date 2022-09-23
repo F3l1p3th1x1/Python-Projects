6a4638a93d2efbc53e7093529216eaf8561e9f53
F3l1p3th1x <46651172+F3l1p3th1x1@users.noreply.github.com>
2022-09-23 13:44:07 -0300

    Create NumberGenerator.py

---
+++ NumberGenerator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(23/09/2022)
+
+@author: %(F3l1p3Th1x3)s
+"""
+
+# THIS CODE GENERATES ALEATORY NUMBERS FROM 0 TO 10000
+
+
+from numpy import random
+
+x = random.randint(10000)
+
+print(x)
