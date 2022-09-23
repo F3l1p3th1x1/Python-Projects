0e367d413a7be1fbeed5ddfbba5f46d669f10bd8
F3l1p3th1x <46651172+F3l1p3th1x1@users.noreply.github.com>
2022-09-23 13:51:21 -0300

    Create Hipotenuse.py

---
+++ Hipotenuse.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: %(F3l1p3Th1x3)s
+"""
+
+# THIS CODE CALCULES DE HYPOTENUSE
+
+# TO USE ADD THE NUMBER OF BASE AND PERP
+
+
+import numpy as np
+
+base = 30
+perp = 20
+
+x = np.hypot(base, perp)
+
+print(x)
